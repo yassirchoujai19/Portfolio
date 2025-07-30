be2d060d76ac2e9b6a1e9d6bb0ef7c648899b457
Yassir Choujai <113991646+yassirchoujai19@users.noreply.github.com>
2025-07-30 19:51:31 +0100

    Initial commit: Add project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./sections/hero"
+
+ 
+const App = () => {
+  return (
+    <>
+    <Hero />
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/components/HeroModels/HeroLights.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroLights = () => {
+  return (
+<>
+ <spotLight position={[2, 5, 6]}
+ intensity={100}
+ />
+ 
+</>
+
+)
+}
+
+export default HeroLights
